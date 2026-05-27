c53236392425946c2566452b2d877d09858bc377
rahernan5 <130753696+rahernan5@users.noreply.github.com>
2026-05-27 12:46:57 +0200

    Arreglar issue 3

--- hipster-core/src/main/java/es/usc/citius/hipster/algorithm/NegativeCycleException.java
+++ hipster-core/src/main/java/es/usc/citius/hipster/algorithm/NegativeCycleException.java
@@ -2,7 +2,7 @@ package es.usc.citius.hipster.algorithm;
 
 
 public class NegativeCycleException extends RuntimeException {
-    private static final String message = "Existence of a negative cycle detected";
+    private static final String MESSAGE = "Existence of a negative cycle detected";
 
     public NegativeCycleException() {
         super(message);
